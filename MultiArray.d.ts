85f292d8464f3c772245e4dd6eb49ca73aead9a0
Preta-Crowz <preta@siro.dev>
2022-11-04 05:18:25 +0900

    update docs

--- MultiArray.d.ts
+++ MultiArray.d.ts
@@ -1,14 +1,101 @@
-export default class MultiArray {
+export class MultiArray {
     constructor (...args: Array<any>[])
+
+    /**
+     * Get element from MultiArray.
+     * 
+     * @param index - Index of element from MultiArray.
+     * 
+     * @throws TypeError
+     * Thrown when argument is not integer.
+     * 
+     * @returns Returns element that was on index. If not exist, returns nothing.
+     */
     get (index: number): any
+
+    /**
+     * Add new body to MultiArray.
+     *
+     * @param args - One or more of Array or MultiArray to added on body.
+     * 
+     * @throws TypeError
+     * Thrown when argument is not Array or MultiArray.
+     * 
+     * @returns New length of body.
+     */
     append (...args: Array<Array|MultiArray>[]): number
+
+    /**
+     * Remove body from MultiArray.
+     *
+     * @param index - Index of body from MultiArray.
+     * 
+     * @throws TypeError
+     * Thrown when argument is not integer.
+     * 
+     * @throws RangeError
+     * Thrown when index is out of range.
+     * 
+     * @returns New length of body.
+     */
     remove (index: number): number
+
+    /**
+     * Length of MultiArray, or sum of length from body.
+     */
     length: number
+
+    /**
+     * Length of MultiArray body, or count of referenced arrays.
+     */
     bodyLength: number
+
+    /**
+     * Found object from MultiArray and returns its index.
+     * 
+     * @param obj - Object to find.
+     * 
+     * @returns Index of object in MultiArray. If not exist, returns -1.
+     */
     indexOf (obj: any): number
+
+    /**
+     * Found object from MultiArray by filter and returns its index.
+     * 
+     * @param func - Filter to use.
+     * 
+     * @returns Index of object in MultiArray. If not exist, returns -1.
+     */
     find (func: Function): number
-    concat (other: Array|MultiArray): MultiArray
+
+    /**
+     * Concat two MultiArray to get new MultiArray.
+     * 
+     * @param other - MultiArray to concat.
+     * 
+     * @throws TypeError
+     * Thrown when argument is not MultiArray.
+     * 
+     * @returns New MultiArray with combined body.
+     */
+    concat (other: MultiArray): MultiArray
+
     toString (): string
+
+    /**
+     * Check the argument can be used as index.
+     * 
+     * @param index - Index to check.
+     * 
+     * @throws TypeError
+     * Thrown when argument is not integer.
+     */
     static checkIndex (index: number): void
+
+    /**
+     * Check the argument is MultiArray or not.
+     * 
+     * @param arr - Object to check.
+     */
     static isMultiArray (arr:any): boolean
 }
